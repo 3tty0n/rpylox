8be806239ed3a0a1585902d50d7fa3a14bd60bb7
Yusuke Izawa <me@yizawa.com>
2024-05-16 21:07:13 +0900

    (24.5.1) support function call in vm

--- lox/vm.py
+++ lox/vm.py
@@ -38,12 +38,25 @@ class CallFrame(object):
         self.ip = ip
         self.slots = slots
 
+    # def pop_slot(self):
+    #     slot_top = jit.promote(self.slot_top)
+    #     w_x = self.slots[slot_top]
+    #     slot_top -= 1
+    #     self.slots[slot_top] = None
+    #     self.slot_top = slot_top
+    #     return w_x
+
+    # def push_slot(self, w_x):
+    #     slot_top = jit.promote(self.stlot_top)
+    #     self.slots[slot_top] = w_x
+    #     self.stack_top = stack_top + 1
+
 
 class VM(object):
     _immutable_fields_ = ['chunk', 'STACK_MAX_SIZE', 'FRAMES_MAX']
 
     global_objects = {}
-    STACK_MAX_SIZE = 8
+    STACK_MAX_SIZE = 16
     FRAMES_MAX = 64
 
     def __init__(self, debug=True):
@@ -59,7 +72,7 @@ class VM(object):
 
         self.frames = [None] * self.FRAMES_MAX
         self.frame_ptr = 0
-        self.current_frame = None
+        self.frame = None
 
         # self.ip = 0 # Moved to CallFrame
 
@@ -91,6 +104,11 @@ class VM(object):
         assert n < stack_top
         return self.stack[stack_top - (n + 1)]
 
+    def _take_stack(self, n):
+        assert n < self.stack_top
+        assert 0 <= n
+        return self.stack[n]
+
     def _trace_stack(self):
         print "       ",
         if self.stack_top == 0:
@@ -99,14 +117,16 @@ class VM(object):
 
         print "[",
         for i in range(self.stack_top):
-            print self.stack[i].repr(),
+            w_x = self.stack[i]
+            if w_x: print w_x.repr(),
+            else: print "None",
         print "]"
 
-        print "       "
+        # print "       ",
         # print self.global_objects
 
-    def _runtime_error(self, message, frame):
-        line_number = format_line_number(frame.function.chunk, frame.ip)
+    def _runtime_error(self, message):
+        line_number = format_line_number(self.frame.function.chunk, self.frame.ip)
         print "%s\n[line %s] in script" % (message, line_number)
         self._reset()
 
@@ -115,6 +135,32 @@ class VM(object):
     #     self.ip = 0
     #     return self.run()
 
+    def _read_byte(self):
+        chunk = self.frame.function.chunk
+        jit.promote(chunk)
+        instruction = chunk.code[self.frame.ip]
+        self.frame.ip += 1
+        return instruction
+
+    def _read_short(self):
+        chunk = self.frame.function.chunk
+        jit.promote(chunk)
+        offset1 = chunk.code[self.frame.ip]
+        offset2 = chunk.code[self.frame.ip + 1]
+        self.frame.ip += 2
+        return offset1 << 8 | offset2
+
+    def _read_constant(self):
+        constant_index = self._read_byte()
+        return self.frame.function.chunk.constants[constant_index]
+
+    def _read_string(self):
+        w_const = self._read_constant()
+        assert isinstance(w_const, ValueObj)
+        obj_str = w_const.get_value()
+        isinstance(obj_str, ObjString)
+        return obj_str
+
     def interpret(self, source):
         self._reset()
 
@@ -122,8 +168,8 @@ class VM(object):
         function = compiler.compile()
         if function:
             self._push_stack(ValueObj(function))
-            frame = CallFrame(function=function, ip=0, slots=self.stack[:])
-            self.frames[self.frame_ptr] = frame
+            self.frame = CallFrame(function=function, ip=0, slots=self.stack[:])
+            self.frames[self.frame_ptr] = self.frame
             self.frame_ptr += 1
             return self.run()
         else:
@@ -132,27 +178,26 @@ class VM(object):
     def run(self):
         instruction = None
         self.frame_ptr -= 1
-        frame = self.frames[self.frame_ptr]
+        self.frame = self.frames[self.frame_ptr]
         if we_are_jitted():
             self.frames[self.frame_ptr + 1] = None
         while True:
             if not we_are_translated():
                 if self.debug_trace:
-                    disassemble_instruction(frame.function.chunk, frame.ip)
+                    disassemble_instruction(self.frame.function.chunk, self.frame.ip)
                     self._trace_stack()
 
-            jitdriver.jit_merge_point(ip=frame.ip, chunk=frame.function.chunk,
+            jitdriver.jit_merge_point(ip=self.frame.ip, chunk=self.frame.function.chunk,
                                       stack=self.stack, stack_top=self.stack_top,
-                                      frame=frame, self=self)
+                                      frame=self.frame, self=self)
 
-            instruction = self._read_byte(frame)
+            instruction = self._read_byte()
             if instruction == OpCode.OP_RETURN:
                 return InterpretResult.INTERPRET_OK
             elif instruction == OpCode.OP_NOP:
                 pass
             elif instruction == OpCode.OP_CONSTANT:
-                w_const = self._read_constant(frame)
-                self._push_stack(w_const)
+                self._constant()
             elif instruction == OpCode.OP_NIL:
                 self._push_stack(ValueNil())
             elif instruction == OpCode.OP_TRUE:
@@ -160,7 +205,7 @@ class VM(object):
             elif instruction == OpCode.OP_FALSE:
                 self._push_stack(ValueBool(False))
             elif instruction == OpCode.OP_NOT:
-                self._push_stack(ValueBool(self._pop_stack().is_falsy()))
+                self._unary_op("!")
             elif instruction == OpCode.OP_EQUAL:
                 self._binary_op("==")
             elif instruction == OpCode.OP_LESS:
@@ -178,129 +223,75 @@ class VM(object):
             elif instruction == OpCode.OP_DIVIDE:
                 self._binary_op("/")
             elif instruction == OpCode.OP_NEGATE:
-                if not self._peek_stack(0).is_number():
-                    self._runtime_error("Runtime error", frame)
-                    raise InterpretRuntimeError()
-                self._push_stack(self._pop_stack().negate())
+                self._unary_op("-")
             elif instruction == OpCode.OP_PRINT:
-                value = self._pop_stack()
-                if isinstance(value, Value) or isinstance(value, Obj):
-                    print value.repr()
-                else:
-                    print value
+                self._print()
             elif instruction == OpCode.OP_POP:
                 self._pop_stack()
             elif instruction == OpCode.OP_DEFINE_GLOBAL:
-                name = self._read_string(frame)
-                assert isinstance(name, ObjString)
-                name_hash = name.hash()
-                self.global_objects[name_hash] = self._pop_stack()
+                self._define_global()
             elif instruction == OpCode.OP_GET_GLOBAL:
-                name = self._read_string(frame)
-                assert isinstance(name, ObjString)
-                name_hash = name.hash()
-                if name_hash not in self.global_objects:
-                    self._runtime_error("Undefined variable '%s'." % name, frame)
-                    raise InterpretRuntimeError()
-                self._push_stack(self.global_objects[name_hash])
+                self._get_global()
             elif instruction == OpCode.OP_SET_GLOBAL:
-                name = self._read_string(frame)
-                assert isinstance(name, ObjString)
-                name_hash = name.hash()
-                if not name_hash in self.global_objects:
-                    self._runtime_error("Undefined variable '%s'." % name, frame)
-                    raise InterpretRuntimeError()
-                self.global_objects[name_hash] = self._pop_stack()
-                self._push_stack(ValueNil()) # To handle with OP_POP
+                self._set_global()
             elif instruction == OpCode.OP_GET_LOCAL:
-                slot = self._read_byte(frame)
-                self._push_stack(frame.slots[slot])
+                self._get_local()
             elif instruction == OpCode.OP_SET_LOCAL:
-                slot = self._read_byte(frame)
-                frame.slots[slot] = self._peek_stack(0)
+                self._set_local()
             elif instruction == OpCode.OP_JUMP_IF_FALSE:
-                offset = self._read_short(frame)
+                offset = self._read_short()
                 if self._peek_stack(0).is_falsy():
-                    frame.ip += offset
+                    self.frame.ip += offset
             elif instruction == OpCode.OP_JUMP:
-                offset = self._read_short(frame)
-                frame.ip += offset
+                offset = self._read_short()
+                self.frame.ip += offset
             elif instruction == OpCode.OP_LOOP: # backward jump
-                offset = self._read_short(frame)
-                frame.ip -= offset
+                offset = self._read_short()
+                self.frame.ip -= offset
                 jitdriver.can_enter_jit(ip=self.ip, chunk=self.chunk,
                                         stack=self.stack, stack_top=self.stack_top,
-                                        frame=frame, self=self)
-            elif instruction == OpCocde.OP_CALL:
-                arg_count = self._read_byte(frame)
-                if not self._call_value(self._peek_stack(arg_count), arg_count, frame):
+                                        frame=self.frame, self=self)
+            elif instruction == OpCode.OP_CALL:
+                arg_count = self._read_byte()
+                if not self._call_value(self._peek_stack(arg_count), arg_count):
                     raise InterpretRuntimeError
             else:
                 print "Unknown opcode"
                 raise InterpretRuntimeError()
 
-    def _read_byte(self, frame):
-        chunk = frame.function.chunk
-        jit.promote(chunk)
-        instruction = chunk.code[frame.ip]
-        frame.ip += 1
-        return instruction
-
-    def _read_short(self, frame):
-        chunk = frame.function.chunk
-        jit.promote(chunk)
-        offset1 = chunk.code[frame.ip]
-        offset2 = chunk.code[frame.ip + 1]
-        frame.ip += 2
-        return offset1 << 8 | offset2
-
-    def _read_constant(self, frame):
-        constant_index = self._read_byte(frame)
-        return frame.function.chunk.constants[constant_index]
-
-    def _read_string(self, frame):
-        w_const = self._read_constant(frame)
-        assert isinstance(w_const, ValueObj)
-        obj_str = w_const.get_value()
-        isinstance(obj_str, ObjString)
-        return obj_str
+    def _unary_op(self, op):
+        if op == "-":
+            if not self._peek_stack(0).is_number():
+                self._runtime_error("Runtime error")
+                raise InterpretRuntimeError()
+            self._push_stack(self._pop_stack().negate())
+        elif op == "!":
+            self._push_stack(ValueBool(self._pop_stack().is_falsy()))
 
     def _binary_op(self, op):
+        w_y = self._pop_stack()
+        w_x = self._pop_stack()
         if op == "+":
-            w_y = self._pop_stack()
-            w_x = self._pop_stack()
             if w_x.is_string() and w_y.is_string():
                 self._concatinate(w_x, w_y)
             elif w_x.is_number() and w_y.is_number():
                 self._push_stack(w_x.add(w_y))
             else:
-                self._runtime_error("Operands must be two numbers or two strings.", frame)
+                self._runtime_error("Operands must be two numbers or two strings.")
                 raise InterpretRuntimeError()
         elif op == "-":
-            w_y = self._pop_stack()
-            w_x = self._pop_stack()
             self._push_stack(w_x.sub(w_y))
         elif op == "*":
-            w_y = self._pop_stack()
-            w_x = self._pop_stack()
             self._push_stack(w_x.mul(w_y))
         elif op == "/":
-            w_y = self._pop_stack()
-            w_x = self._pop_stack()
-            self._push_stack(w_x.div( w_y))
+            self._push_stackt(w_x.div(w_y))
         elif op == "<":
-            w_y = self._pop_stack()
-            w_x = self._pop_stack()
             w_z = ValueBool(w_x.as_number() < w_y.as_number())
             self._push_stack(w_z)
         elif op == ">":
-            w_y = self._pop_stack()
-            w_x = self._pop_stack()
             w_z = ValueBool(w_x.as_number() > w_y.as_number())
             self._push_stack(w_z)
         elif op == "==":
-            w_y = self._pop_stack()
-            w_x = self._pop_stack()
             w_z = ValueBool(w_x.as_number() == w_y.as_number())
             self._push_stack(w_z)
 
@@ -312,15 +303,63 @@ class VM(object):
         w_z = ValueObj(obj_str1.concat(obj_str2))
         self._push_stack(w_z)
 
-    def _call_value(self, callee, arg_count, frame):
-        if isinstance(callee, ObjFunction):
-            return self._call(callee, arg_count)
-        else:
-            self._runtime_error("Can only call functions an d classes.")
+    def _call_value(self, callee, arg_count):
+        if isinstance(callee, ValueObj):
+            objfun = callee.get_value()
+            if isinstance(objfun, ObjFunction):
+                return self._call(objfun, arg_count)
+            else:
+                self._runtime_error("Can only call functions and classes.")
         return False
 
-    def _call(self, function, arg_count, frame):
-        frame.function = function
-        frame.ip = 0
-        frame.slots = self.stack[self.stack_top - arg_count - 1:]
+    def _call(self, function, arg_count):
+        new_frame = CallFrame(function, ip=0,
+                              slots=self.stack[self.stack_top - (arg_count + 1):][:])
+        self.frame = new_frame
+        self.stack = new_frame.slots[:]
+        self.stack_top = arg_count + 1
         return True
+
+    def _set_local(self):
+        slot = self._read_byte()
+        self.stack[slot] = self._peek_stack(0)
+
+    def _set_global(self):
+        name = self._read_string()
+        assert isinstance(name, ObjString)
+        name_hash = name.hash()
+        if not name_hash in self.global_objects:
+            self._runtime_error("Undefined variable '%s'." % name)
+            raise InterpretRuntimeError()
+        self.global_objects[name_hash] = self._pop_stack()
+        self._push_stack(ValueNil()) # To handle with OP_POP
+
+    def _get_local(self):
+        slot = self._read_byte()
+        self._push_stack(self._take_stack(slot + 1))
+
+    def _get_global(self):
+        name = self._read_string()
+        assert isinstance(name, ObjString)
+        name_hash = name.hash()
+        if name_hash not in self.global_objects:
+            self._runtime_error("Undefined variable '%s'." % name)
+            raise InterpretRuntimeError()
+        self._push_stack(self.global_objects[name_hash])
+
+    def _define_global(self):
+        name = self._read_string()
+        assert isinstance(name, ObjString)
+        name_hash = name.hash()
+        self.global_objects[name_hash] = self._pop_stack()
+
+    def _constant(self):
+        w_const = self._read_constant()
+        self._push_stack(w_const)
+
+    def _print(self):
+        value = self._pop_stack()
+        if isinstance(value, Value) or isinstance(value, Obj):
+            print value.repr()
+        else:
+            print value
